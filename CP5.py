48727dfe93ab3b203b9683165400e9f87ce0116b
Matheus <RM99505@fiap.com.br>
2023-10-09 17:53:13 -0300

    commit inicial

---
+++ CP5.py
@@ -0,0 +1,44 @@
+# zfill() preenche uma string com zeros, sendo útil para formatar datas por exemplo
+import datetime
+
+tarefas = []
+
+def verificar_data(a):
+    print(a)
+    try:
+        dia = int(input("Insira o dia:"))
+    except:
+        print("")
+
+def adicionar_tarefa():
+    print("Você selecionou a opção 1. 'Adiconar Tarefa'!")
+    tarefa = {
+        "Nome da tarefa": input("Insira o nome da tarefa"),
+        "Data inicial": verificar_data("Data inicial:")
+    }
+
+def exibir_tarefas():
+    print("Você selecionou a opção 2. 'Exibir Tarefas'!")
+
+def tarefas_atrasadas():
+    print("Você selecionou a opção 3. 'Tarefas Atrasadas'!")
+
+while True:
+    print("GERENCIAMENTO VINHERIA AGNELLO")
+    print("1. Adicionar Tarefa")
+    print("2. Exibir Tarefa(s)")
+    print("3. Tarefas Atrasadas")
+    print("4. Sair")
+    opcao_menu = int(input("Insira o número da opção desejada: "))
+
+    match opcao_menu:
+        case 1:
+            adicionar_tarefa()
+        case 2:
+            exibir_tarefas()
+        case 3: 
+            tarefas_atrasadas()
+        case 4:
+            break
+        case _:
+            print("Opção inválida! Voltando ao menu...")
